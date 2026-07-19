09942d75f0ba974d559f9ef01613753f60d7a44a
ZhouCzor123 <zczornoba@gmail.com>
2026-07-19 16:34:26 -0400

    Update designer name in sidebar markdown

--- app.py
+++ app.py
@@ -303,7 +303,7 @@ st.sidebar.markdown(
     """
     <div style='text-align: center; color: gray; font-size: 0.8em;'>
         <b>Panel Shop Inventory System v2.0</b><br>
-        Designed & Built by <b>Your Name</b><br>
+        Designed & Built by <b>Zhou lung Czornoba</b><br>
         Co-op Term 2026
     </div>
     """, 
